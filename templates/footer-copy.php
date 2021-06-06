04d2ec1188f62c8b58eb61f26608ff0097541dd1
Alex Mangini <alexfrais@gmail.com>
2021-06-06 18:24:54 -0400

    typo lol

--- templates/footer-copy.php
+++ templates/footer-copy.php
@@ -1,9 +1,9 @@
 <?php if ( is_active_sidebar( 'footer-copy' ) ) : ?>
 	<div class="footer-copy">
 		<div class="inner">
-			<?php md_hook_before_foote_copy(); ?>
+			<?php md_hook_before_footer_copy(); ?>
 			<?php dynamic_sidebar( 'footer-copy' ); ?>
-			<?php md_hook_after_foote_copy(); ?>
+			<?php md_hook_after_footer_copy(); ?>
 		</div>
 	</div>
 <?php endif; ?>
