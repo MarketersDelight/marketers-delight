4395edae504490c0781b063cf5fc8d292320e4fc
Alex Mangini <alexfrais@gmail.com>
2021-06-23 16:36:22 -0500

    Disable Drop-ins Add new if update process not yet run

--- lib/admin/settings/dropins/admin-page.php
+++ lib/admin/settings/dropins/admin-page.php
@@ -1,7 +1,7 @@
 <div class="md-dropins md-content-wrap-med">
 	<h2 class="md-title md-sep-small">
 		<?php echo __( 'Drop-ins', 'md' ); ?>
-		<?php if ( file_exists( MD_INSTALLED_DROPINS ) ) : ?>
+		<?php if ( md_setting( array( 'dropins', 'moved_dropins' ) ) : ?>
 			&nbsp;<button id="md_upload_dropin_button" class="button"><?php echo __( 'Add new', 'md' ); ?></button>
 		<?php endif; ?>
 	</h2>
