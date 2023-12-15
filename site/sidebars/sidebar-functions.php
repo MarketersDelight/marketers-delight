e7a89b6dd7bcf858098031ca8f1a4ced2556c789
Alex Mangini <alexfrais@gmail.com>
2023-12-14 18:45:11 -0600

    Check if category of post has custom sidebar

--- site/sidebars/sidebar-functions.php
+++ site/sidebars/sidebar-functions.php
@@ -28,9 +28,6 @@ function md_has_sidebar( $args = array() ) {
 	if ( has_filter( 'md_filter_has_sidebar' ) )
 		return apply_filters( 'md_filter_has_sidebar', $show );
 
-	if ( ! is_active_sidebar( md_get_sidebar_id() ) )
-		return $show;
-
 	$post_type = isset( $args['post_type'] ) ? $args['post_type'] : md_get_post_type();
 	$post_id = isset( $args['post_id'] ) ? $args['post_id'] : get_queried_object_id();
 
@@ -48,10 +45,10 @@ function md_has_sidebar( $args = array() ) {
 	$single = md_meta( array( 'layout', 'sidebar' ), $post_id );
 
 	if ( $global ) {
-		if ( empty( $display['disable'] ) && empty( $single['remove'] ) )
+		if ( ( empty( $display['disable'] ) && ! empty( $single['add'] ) ) || empty( $single['remove'] ) )
 			$show = true;
 	}
-	elseif ( ! empty( $display['enable'] ) || ! empty( $single['add'] ) )
+	elseif ( ( ! empty( $display['enable'] ) && empty( $single['remove'] ) ) || ! empty( $single['add'] ) )
 		$show = true;
 
 	return $show;
@@ -66,9 +63,8 @@ function md_has_sidebar( $args = array() ) {
 function md_get_sidebar_id() {
 	$sidebar = 'sidebar-main';
 
-	if ( is_home() || is_post_type_archive() ) {
+	if ( is_home() || is_post_type_archive() )
 		$sidebar = md_post_type_field( array( 'layout', 'sidebar_archive' ), $sidebar );
-	}
 	elseif ( is_category() || is_tax() ) {
 		$global = md_post_type_field( array( 'layout', 'sidebar_term' ), $sidebar );
 		$sidebar = md_term_meta( array( 'layout', 'custom_sidebar' ), null, $global );
@@ -76,6 +72,26 @@ function md_get_sidebar_id() {
 	elseif ( is_singular() ) {
 		$global = md_post_type_field( array( 'layout', 'sidebar_single' ), $sidebar );
 		$sidebar = md_post_meta( array( 'layout', 'custom_sidebar' ), null, $global );
+
+		if ( md_has_sidebar() && ! $sidebar ) {
+			$term_sidebar = '';
+			$taxonomies = get_taxonomies( array( 'public' => true ) );
+
+			foreach ( $taxonomies as $taxonomy ) {
+				$terms = get_terms( array(
+					'taxonomy' => $taxonomy,
+					'hide_empty' => true
+				) );
+
+				foreach ( $terms as $term ) {
+					$posts_sidebar = md_term_meta( array( 'layout', 'entries_sidebar' ), $term->term_id );
+
+					if ( $posts_sidebar )
+						$sidebar = $posts_sidebar;
+				}
+			}
+		}
+
 	}
 
 	return $sidebar;
