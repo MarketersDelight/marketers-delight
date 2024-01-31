864862df42ccf0eb14a66763ed3c887bcc8d2ecf
Alex Mangini <alexfrais@gmail.com>
2024-01-30 20:06:01 -0600

    class arrangement

--- site/loop/loop-functions.php
+++ site/loop/loop-functions.php
@@ -418,19 +418,41 @@ function md_content( $loop ) {
 	) );
 }
 
+function md_the_loop( $loop, $c ) {
+	if ( ! empty( $loop['featured'] ) && $c <= $loop['featured'] )
+		$loop = md_loop_featured( $loop );
+
+	if ( empty( $loop['read_more'] ) )
+		$loop['read_more'] = __( 'Continue reading &rarr;', 'md' );
+
+	if ( empty( $loop['excerpt_length'] ) )
+		$loop['excerpt_length'] = 55;
+
+	if ( empty( $loop['excerpt_more'] ) )
+		$loop['excerpt_more'] = '[...]';
+
+	if ( get_post_thumbnail_id() ) {
+		$loop['featured_image_id'] = get_post_thumbnail_id();
+
+		if ( ! isset( $loop['featured_image'] ) )
+			$loop['featured_image'] = md_featured_image_position();
+	}
+
+	return $loop;
+}
+
 function md_loop_classes( $loop, $c ) {
-	$classes = array( 'entry' );
 	$loop_style = 'box-style';
 	$disable_box_style = md_setting( array( 'colors', 'design', 'box_style' ) );
+	$classes = array( 'entry' );
 
 	if ( isset( $loop['is_featured'] ) )
 		$classes[] = 'featured';
 	else
 		$classes[] = 'standard';
 
-	if ( $loop['columns'] > 1 )
-		if ( $loop['columns'] <= 5 )
-			$classes[] = 'f' . $loop['columns'];
+	if ( $loop['columns'] > 1 && $loop['columns'] <= 5 )
+		$classes[] = 'f' . $loop['columns'];
 
 	if ( $disable_box_style )
 		$loop_style = '';
--- templates/loop/the-post.php
+++ templates/loop/the-post.php
@@ -3,26 +3,7 @@
 $style = array();
 $cover = md_cover();
 $loop = $looped;
-
-if ( ! empty( $loop['featured'] ) && $c <= $loop['featured'] )
-	$loop = md_loop_featured( $loop );
-
-if ( empty( $loop['read_more'] ) )
-	$loop['read_more'] = __( 'Continue reading &rarr;', 'md' );
-
-if ( empty( $loop['excerpt_length'] ) )
-	$loop['excerpt_length'] = 55;
-
-if ( empty( $loop['excerpt_more'] ) )
-	$loop['excerpt_more'] = '[...]';
-
-if ( get_post_thumbnail_id() ) {
-	$loop['featured_image_id'] = get_post_thumbnail_id();
-
-	if ( ! isset( $loop['featured_image'] ) )
-		$loop['featured_image'] = md_featured_image_position();
-}
-
+$loop = md_the_loop( $loop, $c );
 $classes = md_loop_classes( $loop, $c );
 
 if ( $loop['columns'] > 5 )
