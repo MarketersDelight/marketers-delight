2a17823760014c1475b7254492403dfdeb5a1fe7
Alex Mangini <alexfrais@gmail.com>
2026-09-17 12:11:34 -0500

    remove compile

--- functions.php
+++ functions.php
@@ -1,8 +1,3 @@
 <?php // Run Marketers Delight
 
 require_once trailingslashit( get_template_directory() ) . 'marketers-delight.php';
-
-function vin() {
-	md_compile();
-}
-add_action( 'init', 'vin' );
